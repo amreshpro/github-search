24c2b09fd7c4b880771a28afa0f6ab0bde23911d
Amresh Maurya <amresh.terminal@gmail.com>
2023-10-22 21:47:07 +0530

    remove console log

--- utils/api.js
+++ utils/api.js
@@ -4,9 +4,7 @@ import axios from "axios";
 export const fetchDataFromApi = async (url='amreshpro') => {
 try {
     const { data } = await axios.get(`https://api.github.com/users/${url}`);
-
-    console.log(data);
-  
+ 
     return data; 
 } catch (error) {
     console.log(error)
